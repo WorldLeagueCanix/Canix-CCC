fea87446788a51716b8d430bebdfe3f8c2860526
WorldLeagueCanix <142015114+WorldLeagueCanix@users.noreply.github.com>
2023-08-13 07:02:21 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/18z3fq0sZOiW_kS7nEwS4qv7px2q9gneHcOY6k5pWJ0k/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wrgJu16q0OMZ_dV7BPkrUWC-PRuSzVeC5kCj9fngrIE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBM5NrzLVTPbBJk3f21wZmrTTpKta5Ygrs';
